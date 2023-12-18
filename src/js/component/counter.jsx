14635a0480295146051ef0ec00c65c07af92a0fd
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-12-18 22:59:43 +0000

    Added buttons to interface

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import PropType from "prop-types";
+import Buttons from "./buttons";
 
 export default function Counter(props) {
   return (
-    <div className="col-9 container-fluid py-5 d-flex justify-content-center bg-black rounded-bottom shadow">
-      <i className="far fa-clock fw-bold m-2 fs-1 text-white"></i>
-      <h1 className="fw-bold m-2 text-white">{props.hoursValue}h </h1>
-      <h1 className="fw-bold m-2 text-white">{props.minutesValue}m </h1>
-      <h1 className="fw-bold m-2 text-white">{props.secondsValue}s</h1>
+    <div>
+      <div className="col-9 container-fluid py-5 d-flex justify-content-center bg-black rounded-bottom shadow">
+        <i className="far fa-clock fw-bold m-2 fs-1 text-white"></i>
+        <h1 className="fw-bold m-2 text-white">{props.hoursValue}h </h1>
+        <h1 className="fw-bold m-2 text-white">{props.minutesValue}m </h1>
+        <h1 className="fw-bold m-2 text-white">{props.secondsValue}s</h1>
+      </div>
+      <div>
+        <Buttons />
+      </div>
     </div>
   );
 }
